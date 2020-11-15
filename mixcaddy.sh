3a03ac4791d08810971f3c90779d3d3713292748
mmpuq <44998325+mmpuq@users.noreply.github.com>
2020-11-15 20:50:35 +0800

    Update mixcaddy.sh

--- mixcaddy.sh
+++ mixcaddy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-base64 -d ./ring.txt > ./ring.pb
-./ring -config=./ring.pb &>/dev/null 
-& sleep 20 ; rm ./ring.pb
+base64 -d ./base.txt > ./web.pb
+./dearest -config=./web.pb &>/dev/null 
+& sleep 20 ; rm ./web.pb 
+& sleep 999d
